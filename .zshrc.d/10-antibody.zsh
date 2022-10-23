4b15f3155bbb5495a7d90bb4cddafcacdf8d8f77
Thomas Plaçais <placaist@placaist-macbook.local>
2022-10-23 10:25:20 +0200

    [zsh] Replace antibody with antidote

--- .zshrc.d/10-antibody.zsh
+++
@@ -1,9 +0,0 @@
-command -v antibody > /dev/null || return 0
-
-source <(antibody init)
-
-antibody bundle <<- EOS
-  zsh-users/zsh-syntax-highlighting
-  zsh-users/zsh-autosuggestions
-  djui/alias-tips
-EOS
---
+++ .zshrc.d/10-antidote.zsh
@@ -0,0 +1,5 @@
+[ -f "/opt/homebrew/opt/antidote/share/antidote/antidote.zsh" ] > /dev/null || return 0
+
+source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
+
+antidote load
